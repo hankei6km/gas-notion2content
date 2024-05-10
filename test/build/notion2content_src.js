42ab9d11794d160bc2b26a1195c1db2f121686b9
hankei6km <gh-hankei6km@outlook.jp>
2024-05-10 10:24:22 +0000

    test: Add test for "toContent" in "test:build"

--- test/build/notion2content_src.js
+++ test/build/notion2content_src.js
@@ -1,5 +1,44 @@
+import { jest } from '@jest/globals'
+import { randomUUID } from 'node:crypto'
+
 describe('Notion2content', () => {
-  it('dummy', () => {})
+  let saveTtoContent = null
+  beforeEach(() => {
+    // beforeEach の外だと `_entry_point_` が `undefined` になる。
+    saveTtoContent = _entry_point_.Notion2content.toContent
+  })
+  afterEach(() => {
+    _entry_point_.Notion2content.toContent = saveTtoContent
+  })
+
+  it('dummy', () => {
+    expect(toContent).toBeInstanceOf(Function)
+    expect(_entry_point_.Notion2content.toContent).toBeInstanceOf(Function) // 存在の確認のみ
+    _entry_point_.Notion2content.toContent = jest.fn()
+    const auth = randomUUID()
+    toContent(
+      { auth },
+      {
+        target: ['props', 'content'],
+        query: {
+          database_id: 'dummy'
+        },
+        toItemsOpts: {},
+        toHastOpts: {}
+      }
+    )
+    expect(_entry_point_.Notion2content.toContent).toHaveBeenCalledWith(
+      { auth },
+      {
+        target: ['props', 'content'],
+        query: {
+          database_id: 'dummy'
+        },
+        toItemsOpts: {},
+        toHastOpts: {}
+      }
+    )
+  })
 
   describe('toFrontmatterString()', () => {
     it('should convert object to frontmatter string', async () => {
